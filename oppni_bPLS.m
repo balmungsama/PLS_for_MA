ab05b428baa3d992da0e42a4de1cd0539a059a26
BalmungSama <jbryaneusebio@gmail.com>
2017-08-28 15:39:40 -0400

    added checks to spot errors in input

--- oppni_bPLS.m
+++ oppni_bPLS.m
@@ -4,41 +4,42 @@
 error_quit = 0 ;
 
 if ( exist('OPPNI_dir') == false ) || ( exist(OPPNI_dir,'dir') == false )
-	error('Must enter a valid OPPNI directory') ;
+	error('Must enter a valid OPPNI directory.') ;
 	error_quit = error_quit + 1 ;
 end
 
 if ( exist('BEHAV_data') == false ) || ( exist(BEHAV_data,'dir') == false )
-	error('Must enter a valid behaviour file') ;
+	error('Must enter a valid behaviour file.') ;
 	error_quit = error_quit + 1 ;
 end
 
 if ( exist('OUTPUT_dir') == false ) 
-	error('Must provide an output directory')
+	error('Must provide an output directory.')
 	error_quit = error_quit + 1 ;
 end
 
-if (exist('prefix') == false)
+if (exist('prefix') == false) && (error_quit == 0)
 	prefix = 'output' ;
-	error('No prefix provided. Defaulting to "output" ')
+	error('No prefix provided. Defaulting to "output". ')
 end
 
 if (exist('pipeline') == false) || (pipeline < 1) || (pipeline > 3)
 	error( sprintf(['You must provide a valid pipeline value \n '...
-									'1 = CON \n' ...
-									'2 = FIX \n' ...
-									'3 = IND \n']) )
+									'		1 = CON \n' ...
+									'		2 = FIX \n' ...
+									'		3 = IND \n']) )
 	error_quit = error_quit + 1 ;
 end
 
 if (exist('var_norm') == false) || (var_norm < 0) || (var_norm > 2)
-	error( sprintf(['You must provide a valid pipeline value \n '...
-									'0 = no normalization, directly use input values \n' ...
-									'1 = (column wise) mean centring  of X and Y \n' ...
-									'2 = zscore X and Y  \n']) )
+	error( sprintf(['You must provide a valid pipeline value. \n '...
+									'		0 = no normalization, directly use input values \n' ...
+									'		1 = (column wise) mean centring  of X and Y \n' ...
+									'		2 = zscore X and Y  \n']) )
 	error_quit = error_quit + 1 ;
 end
 
+% check if any errors have occured
 if error_quit > 0
 	exit 
 end
@@ -51,19 +52,28 @@ prefix = [prefix, '_bPLS.mat'] ;
 
 mats.Y = dlmread(BEHAV_data) ;
 if prod( size(mats.Y) ) == 0
-	error('Behaviour file contains no data')
+	error('Behaviour file contains no data.')
 	exit
 end
 
 %% index all cSPM files %%
 
 SPM_dir = fullfile(OPPNI_dir, 'optimization_results', 'spms') ;
+if ( exist(OPPNI_dir,'dir') == false )
+	error('The spm subdirectory does not exist in the OPPNI output directory provided.')
+	exit
+end
 
 cSPMs = dir( fullfile(SPM_dir, '*_sNorm.nii') ) ;
 cSPMs = {cSPMs(:).name}' ;
 
+if length(cSPMs) == 0
+	error('No spm files exist.')
+	exit
+end
+
 if size(mats.Y,1) ~= size(cSPMs,1)
-	error('Behaviour file must have the same number of rows as there are cSPM files')
+	error('Behaviour file must have the same number of rows as there are cSPM files.')
 	exit
 end
 
@@ -77,7 +87,7 @@ for cSPM = cSPMs
 	cSPM = fullfile(SPM_dir, cSPM) ;
 	cSPM = load_nii(cSPM) ;
 	cSPM = cSPM.nii ;
-	cSPM = cSPM
+	cSPM = cSPM(:,:,:, pipeline) ;
 
 	dims = prod( size(cSPM) ) ;
 	dims = [ 1, dims ] ;
